20357149e3f8c6340d2f800b445dabc71642c7c7
Peter Jones <pjones@redhat.com>
2021-02-15 11:41:08 -0500

    Fix an off by one in strnlena()
    I wrote a test case for strnlena() and strndupa() and of course both
    were off by one in the opposite directions...
    
    ... but the next patch obviates the need for them, hopefully, so this
    will wind up getting dropped.

--- include/str.h
+++ include/str.h
@@ -6,7 +6,7 @@ __attribute__((unused))
 unsigned long strnlena(const CHAR8 *s, unsigned long n)
 {
 	unsigned long i;
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 		if (s[i] == '\0')
 			break;
 	return i;
